03a9588ec5bfca1cd7afe0bd6df7a2f6484864fe
bnm9644 <46390980+bnm9644@users.noreply.github.com>
2024-01-08 16:08:27 +0900

    Update custom7.js

--- script/custom7.js
+++ script/custom7.js
@@ -5,7 +5,11 @@
   ★ siblings() - 선택요소의 형제요소 선택,
      siblings(selector) - 선택요소의 형제요소 中, selector 선택
 
-  stop() - 애니메이션 한번 실행시 전에 실행하던건 삭제하는 기능
+     $(this).siblings(".p").addClass("active"); 
+     - this를 제외한 형제 "선택자" 중 .p 요소를 선택하여 그것만 active class 처리한다!
+     - 없으면 형제 전부 다 처리.
+    
+     stop() - 애니메이션 한번 실행시 전에 실행하던건 삭제하는 기능
 */ 
 
 $('.btn span').click(function(){
